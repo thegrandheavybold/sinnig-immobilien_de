945b674e1549039c804cd2e398f921f03ab1d46f
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2022-09-11 19:39:07 +0200

    Update on CMS and Posts

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js/main-min.js");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("admin");
+  //eleventyConfig.addPassthroughCopy("admin");
 
   //11ty Navigation Plugin
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
