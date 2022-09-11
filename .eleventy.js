bc25f08c76f9b534a640a0a37e5519159fdb32cc
christiankriegsheim <39035968+christiankriegsheim@users.noreply.github.com>
2022-09-11 18:05:22 +0200

    Update on CMS

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js/main-min.js");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("./src/admin");
+  //eleventyConfig.addPassthroughCopy("./src/admin");
 
   //11ty Navigation Plugin
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-// Netlify CMS Identity Script
-if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-
 //Shrinking Header on Scroll
  window.addEventListener('scroll', function(){
 
